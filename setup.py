511a5bd8b550819af8948e87a73395c9e08c2e25
Willem Bult <willem.bult@gmail.com>
2014-10-31 19:46:54 -0700

    Prepare for PyPI

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
-import codecs
+
+# convert the readme to pypi compatible rst
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(
     name='celerytest',
-    version='0.1.0',
+    version='0.1',
     author=u'Willem Bult',
     author_email='willem.bult@gmail.com',
     packages=find_packages(),
     url='http://github.com/RentMethod/celerytest',
+    download_url='https://github.com/RentMethod/celerytest/archive/0.1.tar.gz',
+    keywords=['celery','testing','integration','test'],
     license='BSD license, see LICENSE',
-    description='Test functions to use for tests that depend on the execution of Celery tasks',
-    long_description=codecs.open('README.md', 'r', 'utf-8').read(),
+    description='Run a monitored Celery worker for integration tests that depend on Celery tasks',
+    long_description=read_md('README.md'),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
